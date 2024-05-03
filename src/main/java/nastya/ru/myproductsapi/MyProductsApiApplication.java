d5555abdde546c81850c3edaf017f1ca5e07c47f
kannstas <k.annstas@gmail.com>
2024-05-03 15:05:08 +0300

    init commit

---
+++ src/main/java/nastya/ru/myproductsapi/MyProductsApiApplication.java
@@ -0,0 +1,13 @@
+package nastya.ru.myproductsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProductsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyProductsApiApplication.class, args);
+    }
+
+}
